ef302622d49fb6df1f83655c3e51302fccc92b3b
Aldana laurito <c5102076@nwytg.net>
2019-06-04 15:13:19 -0300

    6. Coding your Application

--- app/src/main/java/com/example/qrscanner/MainActivity.java
+++ app/src/main/java/com/example/qrscanner/MainActivity.java
@@ -2,12 +2,107 @@ package com.example.qrscanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Wiring up the Button
+        //Button();
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        //Load the Image
+        /*
+        We do this first by getting a reference to our image view, that we call ‘myImageView'
+         Then you use a BitMapFactory to decode the R.drawable.puppy resources into a Bitmap.
+         Then you set it to be the bitmap for myImageView.
+         */
+        ImageView myImageView = (ImageView) findViewById(R.id.imgview);
+        Bitmap myBitmap = BitmapFactory.decodeResource(
+                getApplicationContext().getResources(),
+                R.drawable.puppy);
+        myImageView.setImageBitmap(myBitmap);
+
+        //In the guide, this code was placed in the Decode the Barcode section,
+        // but since txtView is also used in the Setup the Barcode Detector section I decided tu place it here.
+        TextView txtView = (TextView) findViewById(R.id.txtContent);
+
+
+        //Setup the Barcode Detector
+        /*
+        We create our new BarcodeDetector using a builder,
+        and tell it to look for QR codes and Data Matrices
+        (there are a lot of other barcode types we could also look for).
+         */
+
+        BarcodeDetector detector =
+                new BarcodeDetector.Builder(getApplicationContext())
+                        .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                        .build();
+        if(!detector.isOperational()){
+            txtView.setText("Could not set up the detector!");
+            return;
+        }
+
+        //Detect the Barcode
+        /*
+        This code creates a frame from the bitmap, and passes it to the detector.
+        This returns a SparseArray of barcodes.
+        The API is capable of detecting multiple bar codes in the same frame.
+         */
+        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+        SparseArray<Barcode> barcodes = detector.detect(frame);
+
+        //Decode the Barcode
+        /*
+        Typically in this step you would iterate through the SparseArray,
+        and process each bar code independently.
+        Usually, we need to allow for the possibility that there won't be any barcodes, or there might be several.
+        Because for this sample, I know I have 1 and only 1 bar code, I can hard code for it.
+        To do this, I take the Barcode called ‘thisCode' to be the first element in the array.
+        I then assign it's rawValue to the textView
+         */
+        Barcode thisCode = barcodes.valueAt(0);
+        //TextView txtView = (TextView) findViewById(R.id.txtContent);
+        txtView.setText(thisCode.rawValue);
+
     }
+
+    //Decode the Barcode
+    /*
+        This sets up the event handler (onClick) for when the user presses the button.
+        When they do that, we want to load the bar code, process it for data,
+        and write that data to the TextView.
+    */
+    private void Button(){
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+    }
+
+
 }
